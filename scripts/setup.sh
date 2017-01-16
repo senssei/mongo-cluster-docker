b77752ffc45a946e9bc1b9cfe7bcbaf6f6d548e8
Rafał Warzycha <rafal.warzycha@gmail.com>
2017-01-16 20:00:52 +0100

    Added ENV support in script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+mongodb1=`ping -c 1 ${MONGO1} | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+mongodb2=`ping -c 1 ${MONGO2} | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+mongodb3=`ping -c 1 ${MONGO3} | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+port=${PORT:-27017}
 
 echo "Waiting for startup.."
-until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+until curl http://${mongodb1}:${port}:/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
 
-echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
+echo curl http://${mongodb1}:${port}/serverStatus\?text\=1 2>&1 | grep uptime | head -1
 echo "Started.."
 
-
-echo SETUP.sh time now: `date +"%T" `
-mongo --host ${MONGODB1}:27017 <<EOF
+echo setup.sh time now: `date +"%T" `
+mongo --host ${mongodb1}:${port} <<EOF
    var cfg = {
-        "_id": "my-mongo-set",
+        "_id": "${RS}",
         "members": [
             {
                 "_id": 0,
-                "host": "${MONGODB1}:27017"
+                "host": "${mongodb1}:${port}"
             },
             {
                 "_id": 1,
-                "host": "${MONGODB2}:27017"
+                "host": "${mongodb2}:${port}"
             },
             {
                 "_id": 2,
-                "host": "${MONGODB3}:27017"
+                "host": "${mongodb3}:${port}"
             }
         ]
     };
